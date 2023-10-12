6589c90c9671f102c6e0451c433418b8a46ec80d
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-10-12 14:52:44 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Serkan ADIGÜZEL on 12.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
